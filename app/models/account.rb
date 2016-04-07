2087e1f8191835c0fb9e003a79df758a486b935c
Lei Pi <lpi@memphis.edu>
2016-04-07 19:45:14 +0000

    annotate

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id         :integer          not null, primary key
+#  username   :string
+#  password   :string
+#  level      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Account < ActiveRecord::Base
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id         :integer          not null, primary key
+#  username   :string
+#  password   :string
+#  level      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase

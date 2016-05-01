1a6b722c7415f80ef831dd5daf5588a2d47df8b9
Lei Pi <lpi@memphis.edu>
2016-05-01 11:37:46 -0500

    resolve conflicts

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # == Schema Information
 #
 # Table name: reports
@@ -11,27 +10,8 @@
 #
 
 class Report < ActiveRecord::Base
-    #validations	
+    #validations
     validates :reason, presence: true #format: { with: /\A[a-zA-Z]+\z/ }
     belongs_to :link
     belongs_to :account
 end
-=======
-# == Schema Information
-#
-# Table name: reports
-#
-#  id         :integer          not null, primary key
-#  reason     :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  link_id    :integer
-#
-
-
-class Report < ActiveRecord::Base
-    #validations	
-    validates :reason, presence: true , format: { with: /\A[\w.]*\z/ }
-    belongs_to :link 
-end
->>>>>>> master

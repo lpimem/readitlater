5fb19f0dc72a295287adc067fc4bf58856256ed3
Lei Pi <lpi@memphis.edu>
2016-05-02 13:31:52 -0500

    update model validates

--- app/models/link.rb
+++ app/models/link.rb
@@ -19,6 +19,7 @@ class Link < ActiveRecord::Base
 	validates :title, presence: true
 	validates :level, presence: true,numericality: { greater_than_or_equal_to:1 , less_than_or_equal_to:3}
 	validates :description, presence: true, length: { maximum: 100}
+	validates :user, presence: true
 
 	has_many :reports
 	has_many :ratings

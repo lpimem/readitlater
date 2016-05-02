646320b005622a4e728ca731d00c943998c58c53
Lei Pi <lpi@memphis.edu>
2016-05-02 14:41:22 -0500

    fix seed data error

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,102 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# link seed data
+# account seed data
+account1 = Account.create!(email: "test1@memphis.edu", password: "12345678", sign_in_count: 10, level: 1)
+account2 = Account.create!(email: "test2@memphis.edu", password: "12345678", sign_in_count: 10, level: 0)
+account3 = Account.create!(email: "test3@memphis.edu", password: "12345678", sign_in_count: 10, level: 0)
+account4 = Account.create!(email: "test4@memphis.edu", password: "12345678", sign_in_count: 10, level: 0)
+account5 = Account.create!(email: "admin@memphis.edu", password: "12345678", sign_in_count: 10, level: 0)
+account6 = Account.create!(email: "lwang3@memphis.edu", password: "22222222", sign_in_count: 0, level: 0)
+account_bob = Account.create!(email: "bob@memphis.edu", password: "test_user_12345", sign_in_count: 10, level: 0)
+account_alice = Account.create!(email: "alice@memphis.edu", password: "test_user_12345", sign_in_count: 10, level: 0)
+account_trudy = Account.create!(email: "trudy@memphis.edu", password: "test_user_12345", sign_in_count: 10, level: 0)
+account_lpi = Account.create!(email: "lpi@memphis.edu", password: "12345678", sign_in_count: 10, level: 0)
 
-link1 = Link.create!(url:'https://www.youtube.com/watch?v=TnXXR7IOYiI&feature=youtu.be',title:'Ruby on Rails tutorial',description:'This is a very useful tutorial for creating model class',level:1)
+
+# Account-link seed data (work)
+
+# account1.links << link1
+# account2.links << link2
+# account3.links << link3
+# account4.links << link4
+# account_lpi.links << link5
+# account_alice.links = [link6, link7, link8]
+# account_alice.save!
+
+
+# link seed data
+link1 = Link.create(
+  url:'https://www.youtube.com/watch?v=TnXXR7IOYiI&feature=youtu.be',
+  title:'Ruby on Rails tutorial',
+  description:'This is a very useful tutorial for creating model class',
+  level:1,
+  user: account1
+  )
 link1.created_at = (rand*10).days.ago
 link1.save!
-link2 = Link.create!(url:'https://en.wikipedia.org/wiki/Theory_of_computation',title:'Theory of computation',description:'This link gives information about TOC',level:1)
+link2 = Link.create(
+  url:'https://en.wikipedia.org/wiki/Theory_of_computation',
+  title:'Theory of computation',
+  description:'This link gives information about TOC',
+  level:1,
+  user: account2
+  )
 link2.created_at = (rand*10).days.ago
 link2.save!
-link3 = Link.create!(url:'http://lsa.colorado.edu/papers/dp1.LSAintro.pdf',title:'Latent semantic analysis',description:'It addresses the concept of LSA in detail',level:1)
+link3 = Link.create(
+  url:'http://lsa.colorado.edu/papers/dp1.LSAintro.pdf',
+  title:'Latent semantic analysis',
+  description:'It addresses the concept of LSA in detail',
+  level:1,
+  user: account3
+)
 link3.created_at = (rand*10).days.ago
 link3.save!
-link4 = Link.create!(url:'http://bluebrain.epfl.ch/',title:'Blue Brain Demo',description:'This is a project by IBM',level:1)
+link4 = Link.create(
+  url:'http://bluebrain.epfl.ch/',
+  title:'Blue Brain Demo',
+  description:'This is a project by IBM',
+  level:1,
+  user: account4
+  )
 link4.created_at = (rand*10).days.ago
 link4.save!
-link5 = Link.create!(url:'http://guides.rubyonrails.org/',title:'Learn Ruby in 5 days',description:'A tutorial which can teach you ruby in 5 days',level:1)
+link5 = Link.create(
+  url:'http://guides.rubyonrails.org/',
+  title:'Learn Ruby in 5 days',
+  description:'A tutorial which can teach you ruby in 5 days',
+  level:1,
+  user: account_lpi
+)
 link5.created_at = (rand*10).days.ago
 link5.save!
 
-link6 = Link.create!(url:'https://www.railstutorial.org/',title:'Learn Rails in 3 days Demo',description:'A tutorial which can teach you ruby in 5 days',level:1)
+link6 = Link.create(
+  url:'https://www.railstutorial.org/',
+  title:'Learn Rails in 3 days Demo',
+  description:'A tutorial which can teach you ruby in 5 days',
+  level:1,
+  user: account_alice
+  )
 link6.created_at = (rand*10).days.ago
 link6.save!
-link7 = Link.create!(url:'https://www.railstutorial.org/book',title:'Rails Book',description:'A tutorial which can teach you ruby in 5 days',level:1)
+link7 = Link.create(
+  url:'https://www.railstutorial.org/book',
+  title:'Rails Book',
+  description:'A tutorial which can teach you ruby in 5 days',
+  level:1,
+  user: account_alice
+  )
 link7.created_at = (rand*10).days.ago
 link7.save!
-link8 = Link.create!(url:'https://www.railstutorial.org/book/toy_app',title:'Rails Book Chapter 2',description:'A tutorial which can teach you ruby in 5 days',level:2)
+link8 = Link.create(
+  url:'https://www.railstutorial.org/book/toy_app',
+  title:'Rails Book Chapter 2',
+  description:'A tutorial which can teach you ruby in 5 days',
+  level:2,
+  user: account_alice
+  )
 link8.created_at = (rand*10).days.ago
 link8.save!
 
@@ -45,17 +116,7 @@ rate4 = Rating.create!(value: -1)
 rate5 = Rating.create!(value: 1)
 rate6 = Rating.create!(value: -1)
 
-# account seed data
-account1 = Account.create!(email: "test1@memphis.edu", password: "12345678", sign_in_count: 10, level: 1)
-account2 = Account.create!(email: "test2@memphis.edu", password: "12345678", sign_in_count: 10, level: 0)
-account3 = Account.create!(email: "test3@memphis.edu", password: "12345678", sign_in_count: 10, level: 0)
-account4 = Account.create!(email: "test4@memphis.edu", password: "12345678", sign_in_count: 10, level: 0)
-account5 = Account.create!(email: "admin@memphis.edu", password: "12345678", sign_in_count: 10, level: 0)
-account6 = Account.create!(email: "lwang3@memphis.edu", password: "22222222", sign_in_count: 0, level: 0)
-account_bob = Account.create!(email: "bob@memphis.edu", password: "test_user_12345", sign_in_count: 10, level: 0)
-account_alice = Account.create!(email: "alice@memphis.edu", password: "test_user_12345", sign_in_count: 10, level: 0)
-account_trudy = Account.create!(email: "trudy@memphis.edu", password: "test_user_12345", sign_in_count: 10, level: 0)
-account_lpi = Account.create!(email: "lpi@memphis.edu", password: "12345678", sign_in_count: 10, level: 0)
+
 
 # Followship seed data
 
@@ -76,15 +137,7 @@ link3.ratings << rate6
 account_alice.followers = [account_trudy]
 account_alice.save!
 
-# Account-link seed data (work)
 
-account1.links << link1
-account2.links << link2
-account3.links << link3
-account4.links << link4
-account_lpi.links << link5
-account_alice.links = [link6, link7, link8]
-account_alice.save!
 
 # account-rate seed data
 

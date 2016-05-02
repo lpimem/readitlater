6f15517e56e0d4c5e39901f9ec6599a74573eed9
Lei Pi <lpi@memphis.edu>
2016-05-02 13:10:53 -0500

    refactor tests Use setup to reduce repeating code.

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -13,24 +13,24 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-     
+  setup do
+    @test1 = reports(:test1)
+  end
+
      #reason presence
      test "Report with a reason should be valid" do
-	     test1 =reports(:test1)
-	     assert test1.valid?
+	     assert @test1.valid?
      end
 
      test "Report with empty reason should be invalid" do
-	     test1 =reports(:test1)
-	     test1.reason= ''
-	     assert test1.invalid?
+	     @test1.reason= ''
+	     assert @test1.invalid?
      end
 
     #reason format testing
     test "Link should be invalid if has invalid url" do
-	    test1 = reports(:test1)
-	    test1.reason = '#*()+++=='
-	    assert test1.invalid?
+	    @test1.reason = '#*()+++=='
+	    assert @test1.invalid?
 	end
 
 
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -12,54 +12,50 @@ require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
   MAX_TAG_LENGTH = 36
+  setup do
+      @ruby = tags(:one)
+  end
 
   test "valid tags" do
-    ruby = tags(:one)
-    assert ruby.valid?
+    assert @ruby.valid?
 
     for l in ["1", "a1" * (MAX_TAG_LENGTH / 2), "a_1", "1_a"]
-      ruby.label = l
-      assert ruby.valid?
+      @ruby.label = l
+      assert @ruby.valid?
     end
   end
 
   test "tag label should be longer than 0" do
-    ruby = tags(:one)
-    ruby.label = ""
-    assert ruby.invalid?
+    @ruby.label = ""
+    assert @ruby.invalid?
   end
 
   test "tag label should be less than 37" do
-    ruby = tags(:one)
-    ruby.label = "a" * 37
-    assert ruby.invalid?
+    @ruby.label = "a" * 37
+    assert @ruby.invalid?
   end
 
   test "tags with invalid chars" do
-    ruby = tags(:one)
     for l in [".", ",", " ", ";", "+", "-", "*", "/", "\t", "\r", "\n", ""]
-      ruby .label = l
-      assert ruby.invalid?
+      @ruby .label = l
+      assert @ruby.invalid?
     end
   end
 
   test "tags should have label" do
-    ruby = tags(:one)
-    ruby.label = nil
-    assert ruby.invalid?
+    @ruby.label = nil
+    assert @ruby.invalid?
   end
 
   test "duplicated tags" do
-    ruby = tags(:one)
     assert_raises(Exception){
-      Tag.create!(label=ruby.label)
+      Tag.create!(label=@ruby.label)
     }
   end
 
   test "duplicated tags ignore cases" do
-    ruby = tags(:one)
     assert_raises(Exception){
-      Tag.create!(label=ruby.label.upcase)
+      Tag.create!(label=@ruby.label.upcase)
     }
   end
 

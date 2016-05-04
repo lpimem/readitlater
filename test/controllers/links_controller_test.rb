700de819a10dd8d0c85964bcd50e8802a14ec230
Lei Pi <lpi@memphis.edu>
2016-05-04 00:36:00 -0500

    Fixing unittest for LinkControllers

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -42,23 +42,31 @@ class LinksControllerTest < ActionController::TestCase
   end
 
   test "should show link" do
+    sign_in @user
     get :show, id: @link
     assert_response :success
+    sign_out @user
   end
 
   test "should get edit" do
+    sign_in @user
     get :edit, id: @link
     assert_response :success
+    sign_out @user
   end
 
   test "should update link" do
+    sign_in @user
     patch :update, id: @link, link: { description: @link.description, level: @link.level, title: @link.title, url: @link.url }
     assert_redirected_to link_path(assigns(:link))
+    sign_out @user
   end
 
   test "should destroy link" do
     assert_difference('Link.count', -1) do
+      sign_in @user
       delete :destroy, id: @link
+      sign_out @user
     end
     assert_redirected_to links_path
   end

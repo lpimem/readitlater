97e8c68bb75b6867a14228252afe47d57751bbff
peya hossain <peya hossain>
2016-05-04 21:58:07 -0500

    profile validation added

--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -16,4 +16,20 @@ class ProfileTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "all valid test" do
+   		prog1 = Profile.new
+   		prog1.first_name = "kamrun"
+   		prog1.last_name = "Naher"
+  		assert prog1.valid?
+    end
+  test " valid test for last_name" do
+   		prog1 = Profile.new
+   		prog1.first_name = "Lei"
+  		assert prog1.valid?
+    end
+
+   	test " invalid test for first_name" do
+   		prog1 = Profile.new
+   		prog1.last_name = "pai"
+  		assert prog1.invalid?
 end
